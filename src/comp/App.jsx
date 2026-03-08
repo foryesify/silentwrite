d3c054f1761cec730164f12700dad528cef74481
ForYes <yes-noob@outlook.com>
2026-03-08 21:28:58 +0800

    dev: restart

--- src/comp/App.jsx
+++ src/comp/App.jsx
@@ -1,12 +1,6 @@
 import "../assets/style.css";
-import {MarkdownEditor} from "./Editor";
 
 function App() {
-  return (
-    <main class="container">
-      <MarkdownEditor />
-    </main>
-  );
 }
 
 export default App;
--- src/comp/Titlebar.jsx
+++ src/comp/Titlebar.jsx
@@ -1,3 +1,19 @@
 function Titlebar() {
-    
+    return (
+        <div className="titlebar">
+            <div className="left">
+                <div className="app-title" data-tauri-drag-region></div>
+                <div className="menubar"></div>
+            </div>
+            <div className="center">
+                <div className="page-title" data-tauri-drag-region></div>
+            </div>
+            <div className="right">
+                <div className="settings"></div>
+                <div className="window-buttons"></div>
+            </div>
+        </div>
+    )
 }
+
+export default Titlebar
