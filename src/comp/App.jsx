eb2dd5a482b587946c569e4745b3ff9fcaabc2cc
ForYes <yes-noob@outlook.com>
2026-03-15 14:40:49 +0800

    dev: titlebar

--- src/comp/App.jsx
+++ src/comp/App.jsx
@@ -1,6 +1,10 @@
 import "../assets/style.css";
+import Titlebar from "./Titlebar";
 
 function App() {
+    return (
+        <Titlebar />
+    )
 }
 
 export default App;
--- src/comp/Titlebar.jsx
+++ src/comp/Titlebar.jsx
@@ -1,16 +1,25 @@
+import * as icons from "../icons/window-buttons"
+import { useState } from "preact/hooks"
+import "./Titlebar.css"
+
 function Titlebar() {
+    const windowMaximized = useState(true);
     return (
         <div className="titlebar">
             <div className="left">
                 <div className="app-title" data-tauri-drag-region></div>
                 <div className="menubar"></div>
             </div>
-            <div className="center">
-                <div className="page-title" data-tauri-drag-region></div>
+            <div className="center" data-tauri-drag-region>
+                <div className="page-title"></div>
             </div>
             <div className="right">
                 <div className="settings"></div>
-                <div className="window-buttons"></div>
+                <div className="window-buttons">
+                    <icons.MinimizeIcon />
+                    <icons.MaximizeRestoreIcon />
+                    <icons.CloseIcon />
+                </div>
             </div>
         </div>
     )
